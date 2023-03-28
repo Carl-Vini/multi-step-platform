006f47a2c0fafa082a11e7a286627d8fdff3d166
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-03-27 23:02:20 -0300

    creating the second step

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
 const btnNumbers = document.querySelectorAll('.numbers');
 const step1 = document.getElementsByClassName('step-1');
 const styleSheet = document.styleSheets[0];
+const steps = document.querySelectorAll('.form-step');
 
-console.log(styleSheet);
+console.log(steps)
+console.log(steps[0])
 
 // styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
 // isso torna possível acessar e modificar o aquivo css
 
-btnNumbers.forEach( (element) => {
+btnNumbers.forEach((element) => {
     element.addEventListener('click', (event) => {
         console.log(event.target.textContent)
 
-        if (event.target.textContent != 1 ) {
-            styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
+        if (event.target.textContent == 1) {
+            styleSheet.insertRule('.step-1 { display: block; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-2 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-3 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-4 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-5 { display: none; }', styleSheet.cssRules.length)
         }
+        else if (event.target.textContent == 2) {
+            styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-2 { display: block; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-3 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-4 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-5 { display: none; }', styleSheet.cssRules.length)
+        } 
+        else if (event.target.textContent == 3) {
+            styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-2 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-3 { display: block; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-4 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-5 { display: none; }', styleSheet.cssRules.length)
+        }
+        else if (event.target.textContent == 4){  
+                styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
+                styleSheet.insertRule('.step-2 { display: none; }', styleSheet.cssRules.length)
+                styleSheet.insertRule('.step-3 { display: none; }', styleSheet.cssRules.length)
+                styleSheet.insertRule('.step-4 { display: block; }', styleSheet.cssRules.length)
+                styleSheet.insertRule('.step-5 { display: none; }', styleSheet.cssRules.length)
+        }
+        else {  
+            styleSheet.insertRule('.step-1 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-2 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-3 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-4 { display: none; }', styleSheet.cssRules.length)
+            styleSheet.insertRule('.step-5 { display: block; }', styleSheet.cssRules.length)
+    }
+
     })
 })
 
 
-console.log(btnNumbers);
-console.log(step1); 
+/*console.log(btnNumbers);
+console.log(step1)*/;
 
 
-function hideStep(step) {
-    step.style.display = 'none'
-}
